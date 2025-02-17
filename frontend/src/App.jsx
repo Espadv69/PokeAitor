f374956ad3f8e7b307718ea12085d40c6081a9cf
Espadv69 <aitorjob69@gmail.com>
2025-02-17 13:11:27 +0100

    Add fetching data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,27 @@
-const App = () => {}
+const App = () => {
+  const pokemons = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/pokemons')
+      if (!response.ok) return console.error('Error:', response.statusText)
+
+      const data = await response.json()
+      const $ul = document.querySelector('.pokemons')
+      data.forEach((pokemon) => {
+        const $li = document.createElement('li')
+        $li.textContent = pokemon.name
+        $ul.appendChild($li)
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  
+  pokemons()
+  return (
+    <div>
+      <ul className="pokemons"></ul>
+    </div>
+  )
+}
 
 export default App
