12daf95e256bdd3a0e82c2c04251571f318cf81a
Espadv69 <aitorjob69@gmail.com>
2025-02-17 12:03:39 +0100

    Add welcome route to the Pokemon API

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,9 @@ const pokemons = require('./data/data.js')
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+const PORT = 5000
+
+app.get('/', async (req, res) => {
+  res.send('Welcome to the Pokemon API!')
+})
