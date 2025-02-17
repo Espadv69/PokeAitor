ee2bab3c0678e39b8af5a006b468773e534ebb86
Espadv69 <aitorjob69@gmail.com>
2025-02-17 12:05:12 +0100

    Add app listener, to shows a consoleLog when server is running

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,7 @@ const PORT = 5000
 app.get('/', async (req, res) => {
   res.send('Welcome to the Pokemon API!')
 })
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
