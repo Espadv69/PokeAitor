375f6a2792f487314f4a19e9d6adeedea8e70b54
Espadv69 <aitorjob69@gmail.com>
2025-02-17 12:11:09 +0100

    Change commonjs to modules.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const cors = require('cors')
-const pokemons = require('./data/data.js')
+import express, { json } from 'express'
+import cors from 'cors'
+import pokemons from './data/data.js'
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(json())
 
 const PORT = 5000
 
