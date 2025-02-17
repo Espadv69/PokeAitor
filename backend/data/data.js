a8bcfc53d088d2804b37b4c27460a8445bdacb0c
Espadv69 <aitorjob69@gmail.com>
2025-02-17 10:49:45 +0100

    Add second pokemon, Ivysaur

--- backend/data/data.js
+++ backend/data/data.js
@@ -2,7 +2,13 @@ const pokemons = [
   {
     id: 1,
     name: 'Bulbasaur',
-    type: ['grass', 'poison'],
+    type: ['Grass', 'Poison'],
     img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+  },
+  {
+    id: 2,
+    name: 'Ivysaur',
+    type: ['Grass', 'Poison'],
+    img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
   },
 ]
