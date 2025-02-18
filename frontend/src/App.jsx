ef087c0e1ddec26860faba93366e55e2a6b5c0c5
Espadv69 <aitorjob69@gmail.com>
2025-02-18 09:41:37 +0100

    Fetch Pokémon data using useEffect hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,20 @@ import { useState, useEffect } from 'react'
 const App = () => {
   const [pokemons, setPokemons] = useState([])
 
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/pokemons')
+        if (!response.ok) throw new Error(`Error: ${response.statusText}`)
+
+        const data = await response.json()
+        setPokemons(data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }, [])
+
   return (
     <div>
       <ul className="pokemons"></ul>
