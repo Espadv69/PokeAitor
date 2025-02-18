3ccc5712e3d7ffdd2fbab021448680e5a49fab58
Espadv69 <aitorjob69@gmail.com>
2025-02-18 09:38:22 +0100

    Refactor Pokémon fetching to use React state and effect hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,8 @@
+import { useState, useEffect } from 'react'
+
 const App = () => {
-  const pokemons = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/api/pokemons')
-      if (!response.ok) return console.error('Error:', response.statusText)
+  const [pokemons, setPokemons] = useState([])
 
-      const data = await response.json()
-      const $ul = document.querySelector('.pokemons')
-      data.forEach((pokemon) => {
-        const $li = document.createElement('li')
-        $li.innerHTML = `
-          <p>${pokemon.name}</p>
-          <p>${pokemon.type.join(', ')}</p>
-          <img src="${pokemon.img}" alt="${pokemon.img}" width="100px" />
-        `
-        $ul.appendChild($li)
-      })
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  pokemons()
   return (
     <div>
       <ul className="pokemons"></ul>
